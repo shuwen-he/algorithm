088a41620504c214a524672440c221ab2d709413
Shuwen He <1201220707@pku.edu.cn>
2023-10-03 21:48:19 +0800

    feat: max

---
+++ src/max/max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"please enter two int number:"<<'\n';
+	cin>>a>>b;
+	if(a > b){
+		cout<<"max num is :"<<a<<'\n';
+	}else{
+		cout<<"max num is :"<<b<<'\n';
+	}
+}
