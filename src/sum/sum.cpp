a03675262c6af17a0d8b475c7c0eebff3686ad6a
ShuwenHe <1201220707@pku.edu.cn>
2023-10-03 21:14:31 +0800

    feat: sum

---
+++ src/sum/sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//求a和b两个数之和
+int main(){
+    int a,b,sum;
+    cin>>a>>b;         //输入语句
+    sum = a + b;       //赋值语句
+
+    cout<<"a+b="<<sum<<endl;//输出语句
+
+    return 0;
+    
+
+}
+
