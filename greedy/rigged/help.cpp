fe1f783efc51ccf5e5ec7d804acafe0995aae392
Shuwen He <1201220707@pku.edu.cn>
2023-10-06 13:47:38 +0800

    feat:rigged

--- greedy/rigged/help.cpp
+++ greedy/rigged/help.cpp
@@ -1,46 +1,38 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define nline '\n'
+
+#define ll long long
+
 using namespace std;
 
-void solve()
-{
+void solve(){
     ll n;
     cin >> n;
 
     vector<pair<ll, ll>> arr(n);
 
     for (int i = 0; i < n; i++)
-    {
         cin >> arr[i].first >> arr[i].second;
-    }
 
     bool canWin = true;
 
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i].first >= arr[0].first and arr[i].second >= arr[0].second)
-        {
+    for (int i = 1; i < n; i++){
+        if (arr[i].first >= arr[0].first and arr[i].second >= arr[0].second){
             canWin = false;
         }
     }
 
     if (!canWin)
-    {
-        cout << -1 << nline;
-    }
+        cout << -1<< '\n';
     else
-        cout << arr[0].first << nline;
+        cout << arr[0].first << '\n';
 }
 
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    ll T = 1;
-    cin >> T;
-    while (T--) 
-    {
+
+    ll t = 1;
+    cin >> t;
+    while (t--) 
         solve();
-    }
 }
