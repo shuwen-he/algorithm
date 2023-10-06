0ae37ecfb1c77603cf9793eec690add86bb17cc3
Shuwen He <1201220707@pku.edu.cn>
2023-10-06 10:43:53 +0800

    feat：string

--- gift-carpet/gift-carpet.cpp
+++ gift-carpet/gift-carpet.cpp
@@ -1,54 +1,45 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define nline '\n'
+
 using namespace std;
 
-void solve()
-{
-    ll n, m;
-    cin >> n >> m;
+void solve(){
+	long long n,m;
+	cin>>n>>m;
 
-    char s[n][m];
+	char s[n][m];
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			cin>>s[i][j];
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            cin >> s[i][j];
+	string name = "vika";
+	long long ind = 0,count = 0;
 
-    string name = "vika";
-    ll ind = 0, count = 0;
-    
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            char curr = s[j][i];
-            if (curr == name[ind])
-            {
-                ind++;
-                count++;
-                break;
-            }
-        }
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			char curr = s[j][i];
+			if(curr == name[ind]){
+				ind++;
+				count++;
+				break;
+			}
+		}
 
-        if (count == 4)
-            break;
-    }
+		if(count == 4)
+			break;
+	}
 
-    if (count == 4)
-        cout << "Yes\n";
-    else
-        cout << "No\n";
+	if(count == 4)
+		cout<<"Yes\n";
+	else
+		cout<<"No\n";
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll T = 1;
-    cin >> T;
-    while (T--) 
-    {
-        solve();
-    }
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	long long t = 1;
+	cin>>t;
+	while(t--)
+		solve();
 }
