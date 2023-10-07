98de0ddbca527bb0ba58b5a16d2f1477679c9907
ShuwenHe <1201220707@pku.edu.cn>
2023-10-07 11:42:24 +0800

    feat： greedy

--- greedy/jellyfish-and-game/help.cpp
+++ greedy/jellyfish-and-game/help.cpp
@@ -1,36 +1,30 @@
-/**
- *    author:  tourist
- *    created: 30.09.2023 10:34:57       
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#ifdef LOCAL
-#include "algo/debug.h"
-#else
-#define debug(...) 42
-#endif
-
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
+  int t;
+  cin >> t;
+
+  while (t--) {
     int n, m, k;
     cin >> n >> m >> k;
     vector<int> a(n);
+
     for (int i = 0; i < n; i++) {
       cin >> a[i];
     }
+
     vector<int> b(m);
+
     for (int i = 0; i < m; i++) {
       cin >> b[i];
     }
+
     if (k > 10) {
       k -= (k - 10) / 2 * 2;
     }
+
     for (int it = 0; it < k; it++) {
       sort(a.begin(), a.end());
       sort(b.begin(), b.end());
